3a9268bd8b9bb84037042daa09dde1bb78265a12
Suraj Prakash <surajpr@amdocs.com>
2025-12-24 15:33:07 +0530

    change 2

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Item(BaseModel):
 
 @app.get("/")
 def read_root():
+    print("change 2")
     return {"Hello": "World"}
 
 
@@ -25,3 +26,4 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
